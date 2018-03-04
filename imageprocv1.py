ab93f4f064b13eddaae6809a1ab9b320c614970e
Jeremy <JeremyZihanLi@gmail.com>
2018-03-04 13:45:51 -0800

    tracing finished

--- imageprocv1.py
+++ imageprocv1.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
-import cv2, sys, math, codecs, time
+import cv2, sys, math, codecs, time, copy
+
 #from matplotlib import pyplot as plt
 
 kernel = np.ones((2,2),np.uint8)
@@ -18,10 +19,9 @@ np.array([[0, 0, 1, 0, 0],
 # kernel for closing
 
 
-img = cv2.imread("detailed.png")
-
-
-cv2.imshow("original",img)
+img = cv2.imread("Images/detailed.png")
+#img = cv2.imread("Images/ISIC_0000016.png")
+#cv2.imshow("original",img)
 
 closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, ellipse)
 
@@ -47,10 +47,10 @@ for y in range(len(img)):
 		g+=img[y][x][1]
 		r+=img[y][x][2]
 pix = len(img)*len(img[0])
-r = r/(len(img)*len(img[0]))*0.8
-g = g/(len(img)*len(img[0]))*0.8
-b = b/(len(img)*len(img[0]))*0.8
-print(r,g,b)
+r = r/(len(img)*len(img[0])*1.0)
+g = g/(len(img)*len(img[0])*1.0)
+b = b/(len(img)*len(img[0])*1.0)
+print(b,g,r)
 size = 10
 if(pix < 240 * 240/9*16):
 	size = 4
@@ -85,10 +85,62 @@ for x in range(len(img[0])):
                 currentx = average
 		maxcoordx = x
 print(maxcoordx,maxcoordy)
-
-
-
-#below finds center spot
+run = True
+current = maxcoordx
+newImg=copy.deepcopy(img)
+while(True):
+	if(img[maxcoordy][current][0]<=b and img[maxcoordy][current][1]<=g and img[maxcoordy][current][2]<=r):
+		newImg[maxcoordy][current][0] =0
+	elif(img[maxcoordy+1][current][0]<=b and img[maxcoordy+1][current][1]<=g and img[maxcoordy+1][current][2]<=r):
+                newImg[maxcoordy+1][current][0] =0
+	elif(img[maxcoordy-1][current][0]<=b and img[maxcoordy-1][current][1]<=g and img[maxcoordy-1][current][2]<=r):
+                newImg[maxcoordy-1][current][0] =0
+	else:
+		break
+	#print((img[maxcoordy][current][0],img[maxcoordy][current][1],img[maxcoordy][current][2]))
+	#if current ==0 :
+	#	break
+	current = current - 1
+i = 10000
+prev = [1,0]
+coords = [current+1, maxcoordy]
+print coords
+#[[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
+order = [[-1,0],[-1,-1],[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1]]
+#visited1 =[coords]
+#visited2 = []
+while(i>=0):
+	index = order.index(prev)
+	for x in range((index+1), (index+9)):
+		#print order[x]
+		point = img[coords[1]+order[x%8][1]][coords[0]+order[x%8][0]]
+		#print point
+		#tobreak = False
+		#coords = [coords[0]+order[x%8][0],coords[1]+order[x%8][1]]
+		if(point[0]<=b and point[1]<=g and point[2]<=r):
+			#if(False==(coords in visited1)):
+                             #print "hi"
+                             #print "this:", point
+			     newImg[coords[1]+order[x%8][1]][coords[0]+order[x%8][0]][1] = 0
+			     prev[0] = order[x%8][0]
+			     prev[1] = order[x%8][1]
+			     prev[0]=prev[0]*-1
+			     prev[1]=prev[1]*-1
+			     #print order[x%8]
+			     coords = [coords[0]+order[x%8][0],coords[1]+order[x%8][1]]
+			     #print coords
+			     #if(coords in visited1):
+                             #    visited2.append(coords)
+                             #else:
+                             #   visited1.append(coords)
+			     #visited1.append(coords)
+			     break
+                             #print point
+		
+	i = i-1
+#print coords
+#below finds center spot via checking of dark sq
+"""
 satisfied = False
 #for x in range(10):
 #	print(random.randrange(len(img[0])/4,len(img[0])*3/4,1),random.randrange(len(img)/4,len(img)*3/4,1))
@@ -110,6 +162,8 @@ def findspot(satisfied):
 			return [y,x]
 ar = findspot(satisfied)
 """
+
+"""
 for y in range(len(img)):
         for x in range(len(img[0])):
 		if(img[y][x][0] < b and img[y][x][1] < g and img[y][x][2] < r):
@@ -117,7 +171,8 @@ for y in range(len(img)):
 			img[y][x][1] =0
 			img[y][x][2] =0
 """
-cv2.imshow("imageedit", img)
+
+cv2.imshow("imageedit", newImg)
 
 #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #maxcoordx = 0
