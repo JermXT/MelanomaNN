c8746bd090c5ae299d8a9f914be95d43e80b1bf0
Jeremy <JeremyZihanLi@gmail.com>
2018-03-14 21:35:26 -0700

    test updated

--- test.py
+++ test.py
@@ -1,20 +1,19 @@
 import imageprocOut, sys
 import nnOutput
-total = sys.argv
+#total = sys.argv
 def run(total):
-    print total
+    #print total
     if(len(total) > 2):
         print "too many args"
         return
     const = 0.9
-    answer = imageprocOut.run(total[1],const)
+    answer = imageprocOut.run(total,const)
     while(answer == False):
         const = const+0.1
-        answer = impageprocOut.run(total[1],const)
+        answer = impageprocOut.run(total,const)
     #print answer
     final[0][0] = nnOutput.run([answer])
     if(final >=100):
         return True
     else:
         return False
-run(total)
