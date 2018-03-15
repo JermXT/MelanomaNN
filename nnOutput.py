8cbac5d7df114aaa93b76dafed65f348fd6b4667
Jeremy <JeremyZihanLi@gmail.com>
2018-03-14 22:00:43 -0700

    updated neural net output

--- nnOutput.py
+++ nnOutput.py
@@ -3,11 +3,12 @@ from keras.layers import Dense, Activation
 import hold
 import numpy as np
 from keras.models import load_model
-model = load_model('melanomaModel.h5')
-def run(arr, model):
+#model = load_model('melanomaModel.h5')
+def run(arr):
+	model = load_model('melanomaModel.h5')
         data = np.asarray(arr)
-        print model.predict(data,batch_size=1, verbose=0)
+        return model.predict(data,batch_size=1, verbose=0)
 
 
-print np.asarray(hold.test())
-run(np.asarray(hold.test()),model)
+#print np.asarray(hold.test())
+#run(np.asarray(hold.test()),model)
